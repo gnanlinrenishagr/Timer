b1ce1a1dedbe72fcee5c64e787ade5bca931350a
gnanliinrenishagr <gnaninrenisha@gmail.com>
2023-11-11 09:27:17 +0530

    Agaram Training

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import Apps from './App'
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
@@ -11,7 +11,7 @@ Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+   <Apps/>
     <App/>
   </React.StrictMode>
 );
